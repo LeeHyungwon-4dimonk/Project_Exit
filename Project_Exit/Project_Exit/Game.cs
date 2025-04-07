fcfadb63c0682806a05d9ee19392ba65a2972e46
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-04-07 20:41:44 +0900

    [feat]TitleScene Added
    게임 시스템 틀 잡기, 타이틀 씬의 구현

---
+++ Project_Exit/Project_Exit/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Project_Exit.Scenes;
+
+namespace Project_Exit
+{
+    public static class Game
+    {
+        private static Dictionary<string, BaseScene> sceneDic;
+        private static BaseScene curScene;
+
+        private static bool gameOver = false;
+        public static void Run()
+        {
+            Start();
+
+            while (gameOver == false)
+            {
+                Console.Clear();
+                curScene.Render();
+                curScene.Input();
+                Console.WriteLine();
+                curScene.Update();
+                Console.WriteLine();
+                curScene.Result();
+            }
+
+            End();
+        }
+
+        /// <summary>
+        /// 게임의 초기 설정을 진행
+        /// </summary>
+        private static void Start()
+        {
+            Console.CursorVisible = false;
+            gameOver = false ;
+
+            sceneDic = new Dictionary<string, BaseScene>();
+            sceneDic.Add("Title", new TitleScene());
+
+            curScene = sceneDic["Title"];
+        }
+
+        /// <summary>
+        /// 게임의 마무리 작업 진행
+        /// </summary>
+        private static void End()
+        {
+
+        }
+    }
+}
---
+++ Project_Exit/Project_Exit/Scenes/TitleScene.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Exit.Scenes
+{
+    public class TitleScene : BaseScene
+    {
+        public override void Render()
+        {
+            Console.WriteLine("============================");
+            Console.WriteLine("        Project Exit     ");
+            Console.WriteLine();
+            Console.WriteLine("               시나리오 :"   );
+            Console.WriteLine("                   by 이형원");
+            Console.WriteLine("============================");
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key to Continue...");
+            
+        }
+        public override void Input()
+        {
+            Console.ReadKey(true);
+        }
+
+        public override void Update()
+        {
+;
+        }
+
+        public override void Result()
+        {
+
+        }
+        
+
+        
+    }
+}
